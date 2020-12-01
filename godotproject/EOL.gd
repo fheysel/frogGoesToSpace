e7a10c43857c08e69f32941c14a92277b18ce474
heyselt <15tjh2@queensu.ca>
2020-11-30 22:34:26 -0500

    Reparent Camera2D node to below to the Player tscn
    This allows to have a single camera, and not recreate one for every level
    
    Make EOL object larger, add basic, no asset level2
    
    Add a little more art
    
    Delete empty script file
    
    Update how player detection occurs in EOL scene
    
    Remove debug print statements from EOL scene
    
    Delete duplicate Camera2D node from level1 player

--- godotproject/EOL.gd
+++ godotproject/EOL.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
 func _on_EOL_body_entered(body):
-	if "is_god" in body:
-		print("sda")
+	# Change the current scene to the next level
+	if  body.name == "Player": 
 		get_tree().change_scene("res://Level" + str(int(get_tree().current_scene.name) + 1) + ".tscn")
-		
