d04acc16e27b83d69043df5afb70019e8ad9e9d9
Cooper Harasyn <charasyn@outlook.com>
2020-12-10 14:17:35 -0500

    Add menu to Level1
    My previous commit message was wrong - the menu does work fully.
    I had wanted to make it more "extendable" (complicated) but it works
    fine as-is.
    
    This also removes the unused InGameMenuScreen

--- godotproject/Menus/InGameMenu/InGameMenuScreen.gd
+++
@@ -1,8 +0,0 @@
-extends MarginContainer
-class_name InGameMenuScreen
-
-func _ready():
-    var this = ScriptName           # reference to the script
-    var cppNode = MyCppNode.new()   # new instance of a class named MyCppNode
-
-    cppNode.queue_free()
