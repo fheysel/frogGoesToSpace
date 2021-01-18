1a85b2a27ae5d0b1b9f7cc65e7060d62d34e58c8
heyselt <15tjh2@queensu.ca>
2021-01-18 14:14:15 -0500

    Set up level2 autotile collision regions

--- godotproject/Fan.gd
+++ godotproject/Fan.gd
@@ -2,9 +2,11 @@ extends Area2D
 
 
 func _on_Fan_body_entered(body):
-	body.wind_speed = Vector2(-2, -2)
+	if body.has_method("do_movement"):
+		body.wind_speed = Vector2(-2, -2)
 
 
 func _on_Fan_body_exited(body):
-	body.wind_speed = Vector2(1, 1)
+	if body.has_method("do_movement"):
+		body.wind_speed = Vector2(1, 1)
 	
