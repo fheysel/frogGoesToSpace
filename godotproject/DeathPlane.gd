7171d2cb3cd17b1b3369242ab27d33b82345c8b9
heyselt <15tjh2@queensu.ca>
2021-01-29 16:53:46 -0500

    Add pipe graphic, reimport images with filter off

--- godotproject/DeathPlane.gd
+++ godotproject/DeathPlane.gd
@@ -1,22 +1,6 @@
 extends Area2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_DeathPlane_body_entered(body):
-	if body.has_method("die"):
-		body.die()
-	pass # Replace with function body.
+	if  body.name == "Player":
+		body.takeDamage(1);
+		Global.fade_to_scene("res://Level" + str(int(get_tree().current_scene.name)) + ".tscn")
