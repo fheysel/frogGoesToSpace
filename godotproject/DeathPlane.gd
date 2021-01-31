29dee57e4e7a7bf394eb285d4d705315a601a3b9
heyselt <15tjh2@queensu.ca>
2021-01-31 18:47:28 -0500

    Update deathplane reset, add Nasa at end of big city

--- godotproject/DeathPlane.gd
+++ godotproject/DeathPlane.gd
@@ -1,6 +1,17 @@
 extends Area2D
 
+const level0_start_postion :=  Vector2(121, 896)
+const level1_start_postion := Vector2(121, 256)
+
+var level_name = null
+
 func _on_DeathPlane_body_entered(body):
 	if  body.name == "Player":
-		body.takeDamage(1);
-		Global.fade_to_scene("res://Level" + str(int(get_tree().current_scene.name)) + ".tscn")
+		level_name = Global.get_current_scene_property('level_name')
+		if level_name == "Morning Meadow":
+			Global.fade_set_body_to_position(body, level0_start_postion)
+		elif level_name == "The Big City":
+			Global.fade_set_body_to_position(body, level1_start_postion)
+		
+		body.takeDamage(1)
+		# Global.fade_to_scene("res://Level" + str(int(get_tree().current_scene.name)) + ".tscn")
