d6cee3181d04248913918909182241e382d8355b
heyselt <15tjh2@queensu.ca>
2021-01-31 19:47:36 -0500

    Rework deathplane into single export var

--- godotproject/DeathPlane.gd
+++ godotproject/DeathPlane.gd
@@ -1,19 +1,13 @@
 extends Area2D
 
-export (Vector2) var level0_start_postion
-export (Vector2) var level1_start_postion
-#const level0_start_postion :=  Vector2(121, 896)
-#const level1_start_postion := Vector2(121, 256)
+
+export (Vector2) var start_postion
+
 
 var level_name = null
 
 func _on_DeathPlane_body_entered(body):
 	if  body.name == "Player":
-		level_name = Global.get_current_scene_property('level_name')
-		if level_name == "Morning Meadow":
-			Global.fade_set_body_to_position(body, level0_start_postion)
-		elif level_name == "The Big City":
-			Global.fade_set_body_to_position(body, level1_start_postion)
-		
+		Global.fade_set_body_to_position(body, start_postion)
 		body.takeDamage(1)
 
