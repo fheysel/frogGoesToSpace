13a03648abeb0c7ad554a1408842fc622c7972df
jDenyes <44785745+jDenyes@users.noreply.github.com>
2021-02-02 14:11:04 -0800

    Update AntAttackFire.gd
    change _delta to delta

--- godotproject/AntAttackFire.gd
+++ godotproject/AntAttackFire.gd
@@ -8,12 +8,12 @@ var distance_traveled = 0
 var direction = 1
 var attackDamage = 1
 
-func _physics_process(_delta):
+func _physics_process(delta):
 
 	velocity.x = SPEED * direction
 	velocity.y = 0 # not needed, it's already 0
 	velocity = move_and_slide(velocity, FLOOR)
-	distance_traveled += SPEED * _delta
+	distance_traveled += SPEED * delta
 	
 	if distance_traveled > 200 || is_on_wall():
 		dead()
