2116053e5c4729a81bc02e927a79937af790524a
Cooper Harasyn <charasyn@outlook.com>
2021-02-17 11:26:11 -0500

    Update comments in death plane

--- godotproject/DeathPlane.gd
+++ godotproject/DeathPlane.gd
@@ -9,6 +9,8 @@ var level_name = null
 func _on_DeathPlane_body_entered(body):
 	if  body.name == "Player":
 		body.takeDamage(1)
+		# Check if the player's dead after they take damage
+		# If not, play the animation of them being moved to safe ground
 		if !body.dead:
 			Global.fade_set_body_to_position(body, start_postion)
 
