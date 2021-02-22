6a3a1d2286787e5d25aa94c31d2d67626228004c
Alli Christensen <chrially@gmail.com>
2021-02-21 20:56:15 -0500

    remove additional unnecessary comments

--- godotproject/TextTip.gd
+++ godotproject/TextTip.gd
@@ -1,20 +1,18 @@
 extends Position2D
 
-onready var label = $TextPos/RichTextLabel#get_node("RichTextLabel")
+onready var label = $TextPos/RichTextLabel
 onready var tween = $TextPos/Tween
 
 export (String) var text = "default" #external var
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	 label.set_bbcode(text)
-	 label.set_percent_visible(0.0)#label.set_text("test")# Replace with function body.
+	 label.set_percent_visible(0.0)
 
 
 func _on_PlayerDetector_body_entered(body):	
 	if body.name == "Player":	
 		label.set_percent_visible(1.0)	
-		
 		#text tip fade in
 		tween.start()
 		tween.interpolate_property($TextPos, 'scale', $TextPos.scale, Vector2(1,1), 0.2, Tween.TRANS_LINEAR, Tween.EASE_OUT)
@@ -22,8 +20,6 @@ func _on_PlayerDetector_body_entered(body):
 		
 func _on_PlayerDetector_body_exited(body):	
 	if body.name == "Player":
-		
-			
 		#text tip fade out
 		tween.start()
 		tween.interpolate_property($TextPos, 'scale', Vector2(1,1), Vector2(0.1,0.1), 0.3, Tween.TRANS_LINEAR, Tween.EASE_OUT)
