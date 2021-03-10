7ea3d572cb82ac9de24129cbb843bf2f0ff8ff5b
heyselt <15tjh2@queensu.ca>
2021-03-10 17:11:03 -0500

    Fix unintented Level0.gd changes, add level2 theme music

--- godotproject/Level0.gd
+++ godotproject/Level0.gd
@@ -1,10 +1,10 @@
 extends Node2D
 
 # Each level has to have a name in it now, this will be displayed on the high score screen
-const level_name := "NASA HQ"
+const level_name := "Morning Meadow"
 
-const SPIKE := 0
-const STAR := 1
+const SPIKE := 3
+const STAR := 12
 
 export (PackedScene) var Spike
 export (PackedScene) var Star
@@ -17,9 +17,9 @@ func setup_tiles():
 	# This function takes the level and replaces all the "active tiles" ie 
 	# stars, spikes etc with an instance of their corresponding scene  
 	
-	var cells = $TileMap4.get_used_cells()
+	var cells = $TileMap.get_used_cells()
 	for cell in cells:
-		var index = $TileMap4.get_cell(cell.x, cell.y)
+		var index = $TileMap.get_cell(cell.x, cell.y)
 		match index:
 			SPIKE:
 				create_instance_from_tilemap(cell, Spike, self, Vector2(8, 12))
@@ -30,7 +30,7 @@ func setup_tiles():
 func create_instance_from_tilemap(coord:Vector2, prefab:PackedScene, parent:Node2D, offset:Vector2 = Vector2.ZERO): 
 	# This is where the actual replacement happens. 
 	
-	$TileMap4.set_cell(coord.x, coord.y, -1) # Set the cell to be empty
+	$TileMap.set_cell(coord.x, coord.y, -1) # Set the cell to be empty
 	var pf = prefab.instance() # Create an instance of the scene
-	pf.position = $TileMap4.map_to_world(coord) + offset # Set its position
+	pf.position = $TileMap.map_to_world(coord) + offset # Set its position
 	parent.add_child(pf) # Add it to the level's scene
