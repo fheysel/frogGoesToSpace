5ac141df20d8f8bf27726cfbf5bcc4c25e8d4e5f
Cooper Harasyn <charasyn@outlook.com>
2021-03-13 16:22:15 -0500

    remove garbage

--- godotproject/ground.gd
+++
@@ -1,8 +0,0 @@
-extends StaticBody2D
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var visual = $Visual
-	var collision = CollisionPolygon2D.new()
-	collision.polygon = visual.polygon
-	$".".add_child(collision)
