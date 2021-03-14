0a6cda93f2bd2ff5962ec489ac945e84340fd5fa
heyselt <15tjh2@queensu.ca>
2021-03-14 14:01:48 -0400

    Make scrolling credits screen

--- godotproject/Menus/ThanksForPlayingScreen/ThanksForPlayingScreen.gd
+++ godotproject/Menus/ThanksForPlayingScreen/ThanksForPlayingScreen.gd
@@ -2,7 +2,20 @@ extends MarginContainer
 
 var inhibit_pause = true
 var inhibit_hud = true
+var start = true
+var scroll_speed = 0.5
+
 
 func _process(_delta):
+#	$CenterContainer/VBoxContainer.rect_position.y -= scroll_speed
+#	print($CenterContainer/VBoxContainer.rect_position.y)
+	if(start == true):
+		start = false
+		$VBoxContainer2.rect_position.y = 500
+	
+	if($VBoxContainer2.rect_position.y == -135):
+		scroll_speed = 0
+		
+	$VBoxContainer2.rect_position.y -= scroll_speed
 	if Input.is_action_just_pressed("ui_accept"):
 		Global.fade_to_scene(Global.main_menu_path)
