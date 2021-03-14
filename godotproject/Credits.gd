bd1f6dbbf0a2725d127a5338d28a390751d17cce
heyselt <15tjh2@queensu.ca>
2021-03-14 15:25:44 -0400

    Add space frog to credits scene

---
+++ godotproject/Credits.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var inhibit_pause = true
+var inhibit_hud = true
+
+var hor_speed = 0
+var vert_speed = 0
+var rot_speed = 0.5
+
+var rng = RandomNumberGenerator.new()
+
+func _ready():
+	rng.randomize()
+	hor_speed = rng.randf_range(-1.0, 1.0)
+	vert_speed = rng.randf_range(-1.0, 1.0)
+
+func _process(_delta):
+	if($Sprite.position.x > 700 or $Sprite.position.x < -20):
+			hor_speed = -1 * hor_speed
+	if($Sprite.position.y > 620 or $Sprite.position.y < -20):
+			vert_speed = -1 * vert_speed
+	
+	$Sprite.position.x += hor_speed
+	$Sprite.position.y += vert_speed
+	$Sprite.rotation_degrees += rot_speed
--- godotproject/Menus/ThanksForPlayingScreen/ThanksForPlayingScreen.gd
+++ godotproject/Menus/ThanksForPlayingScreen/ThanksForPlayingScreen.gd
@@ -7,15 +7,14 @@ var scroll_speed = 0.5
 
 
 func _process(_delta):
-#	$CenterContainer/VBoxContainer.rect_position.y -= scroll_speed
-#	print($CenterContainer/VBoxContainer.rect_position.y)
 	if(start == true):
 		start = false
 		$VBoxContainer2.rect_position.y = 500
-	
-	if($VBoxContainer2.rect_position.y == -135):
+		
+	if($VBoxContainer2.rect_position.y == -120):
 		scroll_speed = 0
 		
 	$VBoxContainer2.rect_position.y -= scroll_speed
+	
 	if Input.is_action_just_pressed("ui_accept"):
 		Global.fade_to_scene(Global.main_menu_path)
