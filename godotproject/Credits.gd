9a9b6c2d51b5381f3f108e69bab669999d789f92
heyselt <15tjh2@queensu.ca>
2021-03-14 15:34:33 -0400

    Update boundaries

--- godotproject/Credits.gd
+++ godotproject/Credits.gd
@@ -15,10 +15,12 @@ func _ready():
 	vert_speed = rng.randf_range(-1.0, 1.0)
 
 func _process(_delta):
-	if($Sprite.position.x > 700 or $Sprite.position.x < -20):
+	if($Sprite.position.x > 660 or $Sprite.position.x < -20):
 			hor_speed = -1 * hor_speed
-	if($Sprite.position.y > 620 or $Sprite.position.y < -20):
+			rot_speed = -1 * rot_speed
+	if($Sprite.position.y > 600 or $Sprite.position.y < -20):
 			vert_speed = -1 * vert_speed
+			rot_speed = -1 * rot_speed
 	
 	$Sprite.position.x += hor_speed
 	$Sprite.position.y += vert_speed
