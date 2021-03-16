e968ace59949120374552b6a381ba705c9faaee8
Cooper Harasyn <charasyn@outlook.com>
2021-03-16 13:03:57 -0400

    Update sawblade texture and code

--- godotproject/SawBlade.gd
+++ godotproject/SawBlade.gd
@@ -2,13 +2,11 @@ extends KinematicBody2D
 
 const FLOOR = Vector2(0, -1)
 const MAX_SPEED = 20000
-const MAX_HORIZONTAL_TRAVEL = 100
 const NUM_RAMP_FRAMES = 50
+const SPEED_INC = MAX_SPEED / NUM_RAMP_FRAMES
 
 var velocity = Vector2(0, 0)
 var horizontal_direction = 1
-var horizontal_directional_distance = 0
-var speed_inc = MAX_SPEED / NUM_RAMP_FRAMES
 var ideal_velocity = MAX_SPEED
 
 var attackDamage = 1
@@ -24,12 +22,10 @@ enum MOVE_STATE {
 }
 
 func _ready():
-#	radius = $SawBlade/PlayerCollision.shape.radius
-#	var collNode = get_node("PlayerCollision/CollisionShape2D")
 	radius = $PlayerCollision/CollisionShape2D.shape.radius
 	moveState = MOVE_STATE.accelerating_e
-	
-func _process(delta):
+
+func _physics_process(delta):
 	#calculate is the blade is slowing down, speeding up or at max_speed
 	_calculate_ideal_velocity()
 	
@@ -41,36 +37,30 @@ func _process(delta):
 	var rotation = velocity.x / radius
 	rotation_degrees = int(rotation_degrees + rotation) % 360
 	
-	# if is_on_wall():
-	# 	horizontal_direction *= -1
-		
-	print("IDEAL VELOCITY %f: " % ideal_velocity)
-	print("VELOCITY %f: " % velocity.x)
-	print("DIRECTION %d: " % horizontal_direction)
-	print("MOVE_STATE %d: " % moveState)
-		
 
 func _calculate_ideal_velocity():
 	if moveState == MOVE_STATE.decelerating_e:
-		ideal_velocity = abs(ideal_velocity) - speed_inc
+		ideal_velocity = abs(ideal_velocity) - SPEED_INC
 		if(ideal_velocity <= 0):
 			horizontal_direction *= -1
 			moveState = MOVE_STATE.accelerating_e
 	elif moveState == MOVE_STATE.accelerating_e:
-		ideal_velocity = abs(ideal_velocity) + speed_inc
+		ideal_velocity = abs(ideal_velocity) + SPEED_INC
 		if(ideal_velocity >= MAX_SPEED):
 			moveState = MOVE_STATE.full_speed_e
 	elif moveState == MOVE_STATE.full_speed_e:
 		ideal_velocity = MAX_SPEED
 
 func _on_SawBladeArea_area_exited(area):
-#	horizontal_direction *= -1
-	moveState = MOVE_STATE.decelerating_e
+	# Check that our collision box is the area that exited the SawBladeArea
+	if area == $PlayerCollision:
+		moveState = MOVE_STATE.decelerating_e
 
 func _on_PlayerCollision_body_entered(body):
-	print(body)
-	if (body.name == "Player"):
+	if Global.is_player(body):
 		body.takeDamage(attackDamage)
 
-func take_damage(damage):
+# In order to stop the tongue when it hits the sawblade, we have our "tongue_can_damage" collision bit set.
+# However, we need to have the `take_damage` function, so we just have an empty one here.
+func take_damage(_damage):
 	pass
