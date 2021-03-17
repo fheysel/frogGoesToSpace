677b95e9f737aa818bdd4e909ec47999d1ebe035
heyselt <15tjh2@queensu.ca>
2021-03-17 12:41:08 -0400

    Make tongue stick properly to the platform

--- godotproject/Objects/MovingPlatform/MovingPlatform.gd
+++ godotproject/Objects/MovingPlatform/MovingPlatform.gd
@@ -10,18 +10,19 @@ onready var platform = $Platform
 onready var tween = $MoveTween
 
 var follow = Vector2.ZERO
+var next_pos = Vector2.ZERO
 
 func _ready():
-	print('hi')
 	_init_tween()
 
 func _init_tween():
-	print('sup')
 	var duration = move_to.length() / float(speed * PLATFORM_WIDTH)
 	tween.interpolate_property(self, "follow", Vector2.ZERO, move_to, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, IDLE_DURATION)
 	tween.interpolate_property(self, "follow", move_to, Vector2.ZERO, duration, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT, duration + IDLE_DURATION * 2)
 	tween.start()
 	
-func _physics_process(_delta):
-	platform.position = platform.position.linear_interpolate(follow, 0.075) # Gives some smoothing to the platform
+func _physics_process(delta):
+	next_pos = platform.position.linear_interpolate(follow, 0.075) # Gives some smoothing to the platform
+	$Platform.tongue_stick_velocity = (next_pos - platform.position) / delta
+	platform.position = next_pos
 	
---
+++ godotproject/Objects/MovingPlatform/Platform.gd
@@ -0,0 +1,5 @@
+extends KinematicBody2D
+
+
+var tongue_stick_velocity = 0 # required to slide the tongue with the platform
+							  # controlled by the MovingPlatform script
