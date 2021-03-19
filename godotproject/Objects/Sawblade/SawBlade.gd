681b1c361790de4dba5583ec76b2a6c2b4d7b7a3
Cooper Harasyn <charasyn@outlook.com>
2021-03-19 18:56:22 -0400

    Clean up code and scene file, and use small image

--- godotproject/Objects/Sawblade/SawBlade.gd
+++ godotproject/Objects/Sawblade/SawBlade.gd
@@ -6,11 +6,11 @@ const MIN_SPEED = 1000
 const MAX_HORIZONTAL_TRAVEL = 100
 const NUM_RAMP_FRAMES = 45
 const D_RAMP = 125
+const SPEED_INC = MAX_SPEED / NUM_RAMP_FRAMES
 
 var velocity = Vector2(0, 0)
 var direction = 1
 var horizontal_directional_distance = 0
-var speed_inc = MAX_SPEED / NUM_RAMP_FRAMES
 var ideal_velocity = MAX_SPEED
 
 var attackDamage = 1
@@ -26,38 +26,27 @@ enum MOVE_STATE {
 }
 
 func _ready():
-#	radius = $SawBlade/PlayerCollision.shape.radius
-#	var collNode = get_node("PlayerCollision/CollisionShape2D")
 	radius = $PlayerCollision/CollisionShape2D.shape.radius
 	moveState = MOVE_STATE.accelerating_e
 	position = $StartPos.position;	
 	ideal_velocity = 10
 	totalLength = ($EndPos.position - $StartPos.position).length()
-	print("accelerating")
 
-func _process(delta):
+func _physics_process(delta):
 	#Accelerate the full velocity when leaving one point initally
 	_calculate_ideal_velocity()
-	
-	#Now we do the actual velocity
-	#This is temporary purely for testing speedup logic
-#	velocity.x = ideal_velocity * delta * horizontal_direction
-#	velocity = move_and_slide(velocity, FLOOR)
 
 	#Actual logic:
 	var dir_unit_vector = ($EndPos.position - $StartPos.position).normalized()
 	velocity = dir_unit_vector * ideal_velocity * delta * direction
 	velocity = move_and_slide(velocity, FLOOR)
-	#	find unit vector between the Start and End Position
-	#	(End - Start).normalize()
-	#	velocity.x = unitVector * ideal_velocity * delta * direction
 	
 	#rotating the sprite
 	var rotation = velocity.length() / radius * direction
 	rotation_degrees = int(rotation_degrees + rotation) % 360
 
 func _calculate_ideal_velocity():
-	#Setting originating and ending positions
+	# Setting originating and ending positions
 	var original_position = $StartPos.position;
 	var target_position = $EndPos.position;
 	if (direction == -1):
@@ -67,36 +56,32 @@ func _calculate_ideal_velocity():
 	var dStart = (position - original_position).length()
 	var dEnd = (position - target_position).length()
 	
-	# if acclerating and less than a quarter away from the StartPos keep accelerating
+	# if accelerating and less than a quarter away from the StartPos keep accelerating
 	if (moveState == MOVE_STATE.accelerating_e):
-		ideal_velocity += speed_inc
+		ideal_velocity += SPEED_INC
 		ideal_velocity = min(ideal_velocity, MAX_SPEED)
-#		# once we are a quarter through we stop accelerating the saw
+		# once we are a quarter through we stop accelerating the saw
 		if (dStart  > (D_RAMP)):		
 			moveState = MOVE_STATE.full_speed_e
 	
 	elif (moveState == MOVE_STATE.full_speed_e):
-#		#once we are 3/4 way through we start decelerating
+		# once we are 3/4 way through we start decelerating
 		if (dEnd < D_RAMP):
 			moveState = MOVE_STATE.decelerating_e
 	
 	elif (moveState == MOVE_STATE.decelerating_e):
-		ideal_velocity -= speed_inc
+		ideal_velocity -= SPEED_INC
 		ideal_velocity = max(ideal_velocity, MIN_SPEED)
 		# check to see if we have passed the target point
 		if(dStart > totalLength):
 			direction *= -1
-			moveState = MOVE_STATE.accelerating_e		
-
-func _on_SawBladeArea_area_exited(area):
-	pass
-	#	horizontal_direction *= -1
-	#	moveState = MOVE_STATE.decelerating_e
+			moveState = MOVE_STATE.accelerating_e
 
 func _on_PlayerCollision_body_entered(body):
-	print(body)
-	if (body.name == "Player"):
+	if Global.is_player(body):
 		body.takeDamage(attackDamage)
 
-func take_damage(damage):
+# In order to stop the tongue when it hits the sawblade, we have our "tongue_can_damage" collision bit set.
+# However, we need to have the `take_damage` function, so we just have an empty one here.
+func take_damage(_damage):
 	pass
