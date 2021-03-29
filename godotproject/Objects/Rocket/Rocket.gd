423c226b086f1447104506fc25ea2d1b945b1168
heyselt <15tjh2@queensu.ca>
2021-03-29 14:46:32 -0400

    Make rocket blast off

--- godotproject/Objects/Rocket/Rocket.gd
+++ godotproject/Objects/Rocket/Rocket.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
+var blastOff = false
+var speed = 25
 
 var trauma_level = 0 # counter to see how many jets are on
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+func _process(delta):
+	if(blastOff):
+#		$Sprite.position.y -= speed * delta
+#		$Jet1.position.y -= speed * delta
+		self.position.y -= speed * delta
 
 func activateJet():
 	if trauma_level == 0:
@@ -16,3 +24,10 @@ func activateJet():
 		trauma_level += 1
 		$Jet2.emitting = true
 		$JetBoom.play(0)
+		blastOff = true
+		$Timer.start()
+		
+
+
+func _on_Timer_timeout():
+	speed = 80
